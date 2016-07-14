de1f0f8f41b091c0f6b4c90a4c60bb8c7a46c044
ee5415 <ee5415>
2016-07-14 10:53:27 +0800

    Initial commit

---
+++ app/src/main/java/com/example/rainny_day/MainActivity.java
@@ -0,0 +1,63 @@
+package com.example.rainny_day;
+
+        import android.os.Bundle;
+        import android.support.v7.app.ActionBarActivity;
+        import android.view.View;
+        import android.widget.TextView;
+
+        import java.text.NumberFormat;
+
+/**
+ * This app displays an order form to order coffee.
+ */
+public class MainActivity extends ActionBarActivity {
+
+    int i=0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    /**
+     * This method is called when the order button is clicked.
+     */
+    public void submitOrder(View view) {
+        String PriceMessage="ice coffe\nTotal:"+"$"+(i*50);
+        displayMessage(PriceMessage);
+
+    }
+    public void Decrement(View view) {
+        i--;
+        display(i);
+    }
+    public void Increment(View view) {
+        i++;
+        display(i);
+    }
+
+
+    /**
+     * This method displays the given quantity value on the screen.
+     */
+    private void display(int number) {
+        TextView quantityTextView = (TextView) findViewById(
+                R.id.quantity_text_view);
+        quantityTextView.setText("" + number);
+    }
+    /**
+     * This method displays the given price on the screen.
+     */
+    private void displayPrice(int number) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+    }
+    /**
+     * This method displays the given text on the screen.
+     */
+    private void displayMessage(String message) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(message);
+    }
+}
